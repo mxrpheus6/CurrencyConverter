106c607ffcb964305288cc546ebcadecd8064e15
Pavel Kazachenko <pkazachenkoo@gmail.com>
2024-02-19 20:08:39 +0300

    test

--- src/main/java/com/lab1/converter/repo/CurrencyConverterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lab1.converter.repo;
-
-import com.lab1.converter.entity.CurrencyConverterEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CurrencyConverterRepository extends CrudRepository <CurrencyConverterEntity, Long> {
-}
