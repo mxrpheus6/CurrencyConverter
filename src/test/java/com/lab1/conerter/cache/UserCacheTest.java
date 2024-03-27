5ada9e7220748eca48a80b0e4b5b9f9b811ad933
Pavel Kazachenko <pkazachenkoo@gmail.com>
2024-03-27 18:18:09 +0300

    small fix

--- src/test/java/com/lab1/conerter/cache/UserCacheTest.java
+++ src/test/java/com/lab1/conerter/cache/UserCacheTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class UserCacheTest {
+class UserCacheTest {
     private UserCache userCache;
 
     @BeforeEach
